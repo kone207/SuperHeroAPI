4c1e866387ffec207a26d5dc7583d199482d6d82
kone207 <jonathankone207@gmail.com>
2023-02-09 16:36:12 +0200

    Add project files.

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI.Models;
+using SuperHeroAPI.Services.SuperHeroService;
+
+namespace SuperHeroAPI.Controllers
+{
+     [Route("api/[controller]")]
+     [ApiController]
+   public class SuperHeroController : ControllerBase
+   {
+        private readonly ISuperHeroService _superHeroService;
+
+        public SuperHeroController(ISuperHeroService superHeroService)
+        {
+            _superHeroService = superHeroService;
+        }
+       [HttpGet]
+       public async Task<IActionResult> GetAllHeroes()
+       {
+            var result = _superHeroService.GetAllHeroes();
+           return Ok(result);
+       }
+
+      [HttpGet]
+       [Route("{id}")]
+       public async Task<IActionResult> GetSingleHero(int id)
+       {
+            var result = _superHeroService.GetSingleHero(id);
+            if (result == null)
+            {
+                return NotFound("ID not found");
+            }
+            return Ok(result);
+       }
+
+      [HttpPost]
+       public async Task<IActionResult> AddHero(SuperHero hero)
+       {
+          var result = _superHeroService.AddHero(hero);
+            return Ok(result);
+       }
+
+       [HttpDelete]
+       [Route("id")]
+       public async Task<IActionResult> DeleteHero([FromRoute] int id)
+       {
+            var result = _superHeroService.DeleteHero(id);
+            if (result == null)
+            {
+                return NotFound("ID not found");
+            }
+            return Ok(result);
+       }
+
+       [HttpPut]
+       [Route("{id}")]
+       public async Task<IActionResult> EditHero(int id, SuperHero request)
+       {
+            var result = _superHeroService.EditHero(id, request);
+            if(result == null)
+            {
+                return NotFound("ID not found");
+            }
+            return Ok(result);
+
+       }
+
+   }
+}
---
+++ SuperHeroAPI/Services/SuperHeroService/ISuperHeroService.cs
@@ -0,0 +1,14 @@
+﻿
+namespace SuperHeroAPI.Services.SuperHeroService
+{
+    public interface ISuperHeroService
+    {
+       List<SuperHero> GetAllHeroes();
+       SuperHero? GetSingleHero(int id);
+       List<SuperHero> AddHero(SuperHero hero);
+       List<SuperHero>? DeleteHero(int id);
+       List<SuperHero>? EditHero(int id, SuperHero request);
+
+
+    }
+}
